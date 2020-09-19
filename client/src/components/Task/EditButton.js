2f30b4c5e78e49be136868912468cdfd5c41d154
Nikita Batlis <batlisnikita@gmail.com>
2020-09-19 14:32:48 +0200

    added edit modal

--- client/src/components/Task/EditButton.js
+++ client/src/components/Task/EditButton.js
@@ -1,7 +1,23 @@
 import React from 'react';
+//Material UI
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -13,15 +29,80 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function EditButton() {
-  
+
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
 
   return (
     <div>
       <div>
-        <IconButton aria-label="edit" className={classes.margin}>
-          <EditIcon fontSize="small" />
+        <IconButton aria-label="edit" className={classes.margin} onClick={handleClickOpen}>
+            <EditIcon fontSize="small" />
         </IconButton>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Edit Task</DialogTitle>
+        <DialogContent>
+
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyyy"
+            margin="normal"
+            id="date-picker-inline"
+            label="Select Deadline"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+                'aria-label': 'change date',
+            }}
+            />
+        </MuiPickersUtilsProvider>
+
+        <TextField
+        autoFocus
+        margin="dense"
+        id="name"
+        label="Task description"
+        type="text"
+        variant="outlined"
+        fullWidth
+        />
+        <TextField
+        id="outlined-textarea"
+        label="Notes"
+        multiline
+        variant="outlined"
+        rows={4}
+        fullWidth
+        />
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       </div>
     </div>
   );
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -7,44 +7,175 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 //Icon Import
 import { IconContext } from "react-icons";
-import { GiHamburgerMenu} from 'react-icons/gi';
 import { FiPlus } from 'react-icons/fi';
-
+//Material UI
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+//Components Import
 import TaskList from '../../components/TaskList/TaskList.js'
 
-export default function Dashboard() {
 
-    return(
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(1),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+
+export default function PersistentDrawerLeft() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+    return (
         <div className="parentWrapper">
-            <Container className="menuWrapper">
-                <Row >
-                    <IconContext.Provider value={{ className: "icon" }}>
-                        <Col>
-                            <button className="hamburgerMenu"><GiHamburgerMenu/></button>
-                        </Col>
-                    </IconContext.Provider>
-                </Row>
-            </Container>
+        <div className={classes.root}>
+        <CssBaseline/>
+        
             
-            <Container className="dashboardWrapper">
-
-                <Row className="dashboardHeader">
-                        <Col><h1>Hi, John.</h1></Col>
-                </Row>
-
-                <Row className ="taskListWrapper">
-                    <TaskList />
-                    <TaskList />
-                    <TaskList />
-
-                    <IconContext.Provider value={{ className: "plusIcon" }}>
-                        <Col className="addListCol">
-                            <button><FiPlus/></button>
+            <Container className="menuWrapper">
+                    <Row >
+                        <Col>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                onClick={handleDrawerOpen}
+                                edge="start"
+                                className={clsx(classes.menuButton, open && classes.hide)}
+                            >
+                                <MenuIcon />
+                            </IconButton>
                         </Col>
-                    </IconContext.Provider>
-                </Row>
-                
-            </Container>
+                    </Row>
+                </Container>
+            
+
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                <IconButton onClick={handleDrawerClose}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+                </div>
+                <Divider />
+                <List>
+                {['Account', 'Settings', 'Notifications'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+                </List>
+                <Divider />
+                <div className="drawerBtn">
+                    <button className="orangeButton">Logout</button>
+                </div>
+            </Drawer>
+
+
+            <main
+                className={clsx(classes.content, {
+                [classes.contentShift]: open,
+                })}
+                >
+                <Container className="dashboardWrapper">
+
+                    <Row className="dashboardHeader">
+                            <Col><h1>Hi, John.</h1></Col>
+                    </Row>
+
+                    <Row className ="taskListWrapper">
+                        <TaskList />
+                        <TaskList />
+                        <TaskList />
+                        <IconContext.Provider value={{ className: "plusIcon" }}>
+                            <Col className="addListCol">
+                                <button><FiPlus/></button>
+                            </Col>
+                        </IconContext.Provider>
+                    </Row>
+                </Container>
+            </main>
+        </div>
         </div>
     )
 }
+
+
+
