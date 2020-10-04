6b4eece1c201de3fd5fb8d517954e9bbe360eac5
Nikita Batlis <batlisnikita@gmail.com>
2020-10-04 21:52:33 +0200

    update config

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+module.exports ={
+	google: {
+		clientID: '385363165999-ka2qkri1bbg059j6uoao3dfq0i2rv5tl.apps.googleusercontent.com',
+		clientSecret: 'TqPHKi2Jot3TnXDLh7Vg3Sev'
+    },
+    facebook: {
+        appID: '1199843197055992',
+        appSecret: '60f59314808bfc47e6b48d326b7065a6'
+    },
+	mongoDB: {
+		dbURI: "mongodb+srv://NikitaB:projectpassword@my-cluster.rrojr.mongodb.net/db_test?w=majority"
+	},
+	session: {
+		cookieKey: 'webclient1'
+	}
+}
